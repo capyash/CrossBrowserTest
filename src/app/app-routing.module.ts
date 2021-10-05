9456778ffd2c39a04a3814169f2435f406670bae
Yash Capoor <yash.capoor@soroco.com>
2021-10-05 14:41:41 +0530

    Add basic component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SimpleFormComponent } from './components/simple-form/simple-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: SimpleFormComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
